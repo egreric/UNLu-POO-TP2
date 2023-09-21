28fcaff041c9b672092a9bf1788b542b17b557e5
egreric <eric.egr@gmail.com>
2023-09-21 16:19:50 -0300

    correccion ejercicio distribuidora

--- src/ar/edu/unlu/poo/tp2/ejercicioDistribuidora/Stock.java
+++ src/ar/edu/unlu/poo/tp2/ejercicioDistribuidora/Stock.java
@@ -3,7 +3,8 @@ package ar.edu.unlu.poo.tp2.ejercicioDistribuidora;
 import java.util.ArrayList;
 
 public class Stock {
-    ArrayList<Articulo> listadoStock = new ArrayList<>();
+    private ArrayList<Articulo> listadoStock = new ArrayList<>();
+
 
 
 }
